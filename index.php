66384402e94d1be774015d1b5e241ac8a288b64a
Dmitry Kalenyuk <valters96@gmail.com>
2017-12-10 22:37:19 +0200

    rebased folders

---
+++ index.php
@@ -0,0 +1,253 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>BeefClub | Главная</title>
+	<!-- custom-theme -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+	<link rel="stylesheet" href="maincss/lightbox.css">
+
+	<!-- //custom-theme files-->
+	<link href="maincss/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="maincss/aos.css" rel="stylesheet" type="text/css" media="all" /><!-- //animation effects-css-->
+	<link href="maincss/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- //custom-theme files-->
+
+	<!-- font-awesome-icons -->
+	<link href="maincss/font-awesome.css" rel="stylesheet">
+	<!-- //font-awesome-icons -->
+
+	<!-- googlefonts -->
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+	<!-- //googlefonts -->
+
+</head>
+<body>
+
+<!-- banner -->
+<div id="home" class="w3ls-banner">
+	<!-- banner-text -->
+	<div class="slider">
+		<div class="callbacks_container">
+			<ul class="rslides callbacks callbacks1" id="slider4">
+				<li>
+					<div class="w3layouts-banner-top">
+						<div class="banner-dott">
+							<div class="container">
+								<div data-aos="fade-left" class="agileits-banner-info">
+									<h3>Beef Club</h3>
+									<p>Мясной ресторан</p>
+									<div class="agileits_w3layouts_more menu__item">
+										<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">Learn More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="w3layouts-banner-top w3layouts-banner-top1">
+						<div class="banner-dott">
+							<div class="container">
+								<div data-aos="fade-left" class="agileits-banner-info">
+									<h3>Beef Club</h3>
+									<p>Мясной ресторан</p>
+									<div class="agileits_w3layouts_more menu__item">
+										<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">Learn More</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="w3layouts-banner-top w3layouts-banner-top2">
+						<div class="banner-dott">
+							<div class="container">
+								<div data-aos="fade-left" class="agileits-banner-info">
+									<h3>Beef Club</h3>
+									<p>Мясной ресторан</p>
+									<div class="agileits_w3layouts_more menu__item">
+										<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">Learn More</a>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+		<div class="clearfix"> </div>
+
+		<!--banner Slider starts Here-->
+	</div>
+	<div class="thim-click-to-bottom">
+		<div class="rotate">
+			<a href="#about" class="scroll">
+				<i class="fa fa-long-arrow-down" aria-hidden="true"></i>
+			</a>
+		</div>
+	</div>
+
+</div>
+<!-- //banner -->
+<!-- header -->
+<div class="header-w3layouts">
+	<!-- Navigation -->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header page-scroll">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">FoodChef</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<h1><a class="navbar-brand" href="/"><span>Beef</span>Club</a></h1>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+					<li class="hidden"><a class="page-scroll scroll" href="#page-top"></a>	</li>
+					<li><a href="/restaurant/">|Ресторан|</a></li>
+					<li><a href="/bar/">|Бар|</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container -->
+	</nav>
+</div>
+<!-- //header -->
+
+
+
+
+
+		<!-- bootstrap-modal-pop-up -->
+		<div class="modal video-modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						Food Chef
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					</div>
+					<div class="modal-body">
+						<img src="mainimages/food3.jpg" alt=" " class="img-responsive" />
+						<p>Ut enim ad minima veniam, quis nostrum
+							exercitationem ullam corporis suscipit laboriosam,
+							nisi ut aliquid ex ea commodi consequatur? Quis autem
+							vel eum iure reprehenderit qui in ea voluptate velit
+							esse quam nihil molestiae consequatur, vel illum qui
+							dolorem eum fugiat quo voluptas nulla pariatur.
+							<i>" Quis autem vel eum iure reprehenderit qui in ea voluptate velit
+								esse quam nihil molestiae consequatur.</i></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- //bootstrap-modal-pop-up -->
+
+
+		<script src="mainjs/lightbox-plus-jquery.min.js"> </script><!-- for gallery js -->
+
+		<!-- js -->
+		<script type="text/javascript" src="mainjs/jquery-2.1.4.min.js"></script>
+		<!-- for bootstrap working -->
+		<script src="mainjs/bootstrap.js"></script>
+		<!-- //for bootstrap working -->
+		<!-- //js -->
+
+		<!-- /Responsive slides js -->
+		<script src="mainjs/responsiveslides.min.js"></script>
+		<script>
+            // You can also use "$(window).load(function() {"
+            $(function () {
+                // Slideshow 4
+                $("#slider4").responsiveSlides({
+                    auto: true,
+                    pager:true,
+                    nav:false,
+                    speed: 500,
+                    namespace: "callbacks",
+                    before: function () {
+                        $('.events').append("<li>before event fired.</li>");
+                    },
+                    after: function () {
+                        $('.events').append("<li>after event fired.</li>");
+                    }
+                });
+
+            });
+		</script>
+		<script>
+            // You can also use "$(window).load(function() {"
+            $(function () {
+                // Slideshow 4
+                $("#slider3").responsiveSlides({
+                    auto: true,
+                    pager:false,
+                    nav:true,
+                    speed: 500,
+                    namespace: "callbacks",
+                    before: function () {
+                        $('.events').append("<li>before event fired.</li>");
+                    },
+                    after: function () {
+                        $('.events').append("<li>after event fired.</li>");
+                    }
+                });
+
+            });
+		</script>
+
+		<!-- Responsive slides js -->
+
+		<!-- animation effects-js files-->
+		<script src="mainjs/aos.js"></script><!-- //animation effects-js-->
+		<script src="mainjs/aos1.js"></script><!-- //animation effects-js-->
+		<!-- animation effects-js files-->
+
+		<!-- //here starts scrolling icon -->
+		<script src="mainjs/SmoothScroll.min.js"></script>
+		<script type="text/javascript" src="mainjs/move-top.js"></script>
+		<script type="text/javascript" src="mainjs/easing.js"></script>
+		<!-- here stars scrolling script -->
+		<script type="text/javascript">
+            $(document).ready(function() {
+                /*
+					var defaults = {
+					containerID: 'toTop', // fading element id
+					containerHoverID: 'toTopHover', // fading element hover id
+					scrollSpeed: 1200,
+					easingType: 'linear'
+					};
+				*/
+
+                $().UItoTop({ easingType: 'easeOutQuart' });
+
+            });
+		</script>
+		<!-- //here ends scrolling script -->
+		<!-- //here ends scrolling icon -->
+
+		<!-- scrolling script -->
+		<script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $(".scroll").click(function(event){
+                    event.preventDefault();
+                    $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+                });
+            });
+		</script>
+		<!-- //scrolling script -->
+
+</body>
+</html>
